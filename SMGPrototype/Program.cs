950e2cde279c9b3dd2f5890b50afc91576b25e31
Aravind Venkata Punugu <tannishpage1@gmail.com>
2020-06-05 13:19:53 +1000

    Uploading Project Files
    Also added an example of webpage downloading into the Program.cs file

---
+++ SMGPrototype/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace SMGPrototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example to download a webpage
+            WebClient x = new WebClient();
+            String y = x.DownloadString("http://google.com");
+            Console.WriteLine(y);
+            Console.ReadKey();
+        }
+    }
+}
